78e989ebc01b4613360c2cbfff47d57acc1912fb
abalabin <abalabin@student.ucode.world>
2020-10-22 13:22:48 +0300

    refactor FileTag

--- app/src/FileTag.cpp
+++ app/src/FileTag.cpp
@@ -2,53 +2,48 @@
 
 FileTags::FileTags(const std::string &path, QString &fileName) {
     std::string fullPath(path + fileName.toStdString());
-    this->setInfo("filename", fileName);
-    this->setInfo("path", QString::fromStdString(fullPath));
-    TagLib::FileRef f(fullPath.c_str());
-    m_fileRef = f;
-    if (!f.isNull() && f.tag()) {
-        TagLib::Tag *tag = f.tag();
-        this->setInfo("title", tag->title().toCString());
-        this->setInfo("artist", tag->artist().toCString());
-        this->setInfo("album", tag->album().toCString());
-        this->setInfo("genre", tag->genre().toCString());
-        this->setInfo("year", QString::number(tag->year()));
-        this->setInfo("track_number", QString::number(tag->track()));
+    tags.filename = fileName;
+    tags.path = QString::fromStdString(fullPath);
+    m_fileRef = TagLib::FileRef(fullPath.c_str());
+
+    if (!m_fileRef.isNull() && m_fileRef.tag()) {
+        TagLib::Tag *tag = m_fileRef.tag();
+
+        tags.name = tag->title().toCString();
+        tags.artist = tag->artist().toCString();
+        tags.album = tag->album().toCString();
+        tags.genre = tag->genre().toCString();
+        tags.year = QString::number(tag->year());
+        tags.trackNumber = QString::number(tag->track());
     }
 }
 
 std::ostream &operator<<(std::ostream &out, const FileTags &file) {
-    for(const auto& [key, value] : file.getAllInfo()) {
-        std::cout << key << ": " << value.toStdString() << std::endl;
-    }
+        qDebug() << file.tags.filename<< "\n"
+                        << file.tags.path << "\n"
+                        << file.tags.name << "\n"
+                        << file.tags.artist << "\n"
+                        << file.tags.album << "\n"
+                        << file.tags.genre << "\n"
+                        << file.tags.year << "\n"
+                        << file.tags.trackNumber;
     return out;
 }
 
-void FileTags::setInfo(const std::string &tag, const QString &info) {
-    m_file_tags[tag] = info;
-}
-
-QString FileTags::getTag(const std::string &tag){
-    return m_file_tags[tag];
-}
-
-std::map<std::string, QString> FileTags::getAllInfo() const {
-    return m_file_tags;
-}
-
 void FileTags::upgradeFileTags(const FileTags &new_tags) {
-    TagLib::FileRef file(new_tags.getAllInfo()["path"].toStdString().c_str());
-    file.tag()->setTitle(new_tags.getAllInfo()["title"].toStdString());
-    setInfo("title", new_tags.getAllInfo()["title"]);
-    file.tag()->setArtist(new_tags.getAllInfo()["artist"].toStdString());
-    setInfo("artist", new_tags.getAllInfo()["artist"]);
-    file.tag()->setAlbum(new_tags.getAllInfo()["album"].toStdString());
-    setInfo("album", new_tags.getAllInfo()["album"]);
-    file.tag()->setGenre(new_tags.getAllInfo()["genre"].toStdString());
-    setInfo("genre", new_tags.getAllInfo()["genre"]);
-    file.tag()->setYear(new_tags.getAllInfo()["year"].toInt());
-    setInfo("year", new_tags.getAllInfo()["year"]);
-    file.tag()->setTrack(new_tags.getAllInfo()["track_number"].toInt());
-    setInfo("track_number", new_tags.getAllInfo()["track_number"]);
+    TagLib::FileRef file(new_tags.tags.path.toStdString().c_str());
+
+    file.tag()->setTitle(new_tags.tags.name.toStdString());
+    tags.name = new_tags.tags.name;
+    file.tag()->setArtist(new_tags.tags.artist.toStdString());
+    tags.artist = new_tags.tags.artist;
+    file.tag()->setAlbum(new_tags.tags.album.toStdString());
+    tags.album = new_tags.tags.album;
+    file.tag()->setGenre(new_tags.tags.genre.toStdString());
+    tags.genre = new_tags.tags.genre;
+    file.tag()->setYear(new_tags.tags.year.toInt());
+    tags.year = new_tags.tags.year;
+    file.tag()->setTrack(new_tags.tags.trackNumber.toInt());
+    tags.trackNumber = new_tags.tags.trackNumber;
     file.save();
 }
--- app/src/FileTag.h
+++ app/src/FileTag.h
@@ -12,15 +12,24 @@
 #include <mpegfile.h>
 #include <id3v2tag.h>
 #include <QString>
+#include <QDebug>
 
 #include "ImageFile.h"
 
 class FileTags {
     public:
+    struct  Tags{
+        QString filename;
+        QString path;
+        QString name;
+        QString artist;
+        QString album;
+        QString genre;
+        QString year;
+        QString trackNumber;
+    } tags;
+
     FileTags(const std::string& path, QString& fileName);
-    void setInfo(const std::string& tag, const QString& info);
-    QString getTag(const std::string& tag);
-    [[nodiscard]] std::map<std::string, QString> getAllInfo() const;
     void upgradeFileTags(const FileTags& new_tags);
     friend std::ostream& operator<<(std::ostream& out, const FileTags& file);
 
