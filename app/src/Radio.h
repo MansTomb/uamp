b9fa65db418a7d522da9f30be9364b4169bca863
abalabin <abalabin@student.ucode.world>
2020-11-10 13:26:09 +0200

    Added radio

--- app/src/Radio.h
+++ app/src/Radio.h
@@ -1,22 +1,37 @@
-#ifndef RADIO_H
-#define RADIO_H
+#pragma once
 
 #include <QDialog>
+#include <QDebug>
+#include <QMessageBox>
+
+#include "bass.h"
+#include "ui_Radio.h"
 
 namespace Ui {
 class Radio;
 }
 
-class Radio : public QDialog
-{
-    Q_OBJECT
+class Radio : public QDialog {
+ Q_OBJECT
 
-public:
+ public:
     explicit Radio(QWidget *parent = nullptr);
     ~Radio();
 
-private:
-    Ui::Radio *ui;
-};
+ public slots:
+    void OnPlayPause() {if (m_play) BASS_ChannelPlay(m_stream, false); else BASS_ChannelPause(m_stream);m_play = !m_play;};
+    void OnVolumeChanged(int value) {BASS_ChannelSetAttribute(m_stream,BASS_ATTRIB_VOL,static_cast<float>(value) / 100);};
+    void OnConnect() {
+        m_stream = BASS_StreamCreateURL(ui->lineEdit->text().toLatin1(), 0, 0, nullptr, nullptr);
+        if (m_stream == 0)
+            QMessageBox::about(this, "Wrong URL", "Cant connect to this url!");
+        else
+            QMessageBox::about(this, "Connected", "Succesfully connected!");
+    };
 
-#endif // RADIO_H
+ private:
+    Ui::Radio *ui;
+
+    bool m_play{true};
+    HSTREAM m_stream;
+};
